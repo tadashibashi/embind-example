bbe6f020fae53ebab6b61ef04b525884f8b533a4
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-09-03 19:40:53 -0700

    install typescript, create ts driver file

---
+++ main.ts
@@ -0,0 +1,30 @@
+// Add '& EmscriptenModule' because the type info generated via embind does not include
+// the Emscripten module information
+import Module from "./lib/embind_test/embind_test";
+
+const M = Module as typeof Module & EmscriptenModule;
+
+
+// Wait until module has been initialized to...
+M.onRuntimeInitialized = function() {
+
+    // Instantiate C++ class
+    const myClass = new Module.MyClass("hello world!", 0);
+
+    // Call class functions
+    myClass.incInt();
+    myClass.incInt();
+    myClass.incInt();
+    myClass.incInt();
+
+    // Print result: 4
+    console.log(myClass.int);
+    console.assert(myClass.int === 4);
+
+    // Call static function
+    console.log(Module.MyClass.getStringFromInstance(myClass));
+    console.assert(Module.MyClass.getStringFromInstance(myClass) === "hello world!");
+
+    // Make sure to clean up C++ class!
+    myClass.delete();
+};
